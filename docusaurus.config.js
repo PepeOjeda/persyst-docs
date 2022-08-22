383d9ad773b9de6db70c35974a7004c43c7541b6
Pepe Ojeda <Ojedamorala@hotmail.com>
2022-08-22 23:02:21 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Persyst Documentation',
   tagline: 'Serialization plugin for Unity',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://pepeojeda.github.io/',
+  baseUrl: '/persyst-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/logo.ico',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'PepeOjeda', // Usually your GitHub org/user name.
-  projectName: 'Persyst', // Usually your repo name.
+  projectName: 'persyst-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
